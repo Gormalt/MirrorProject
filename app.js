0db04a60bf584b32bd46c105515e5284be04c72b
Kyle Robert van Winkoop <kylevanwinkoop@gmail.com>
2022-02-05 15:54:50 -0800

    Testing Stuff

--- app.js
+++ app.js
@@ -1,56 +1,53 @@
+//Got the packages (in the form of objects)
 var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
 
+//Send the user the 'index.html' file, to let them interact
 app.get('/',function(req, res){
 	res.sendFile(__dirname + '/client/index.html');
 });
 app.use('/client', express.static(__dirname + '/client'));
 
+//Start the server on a port
 serv.listen(8012);
 console.log("Server started.");
-var SOCKET_LIST = {};\
 
+//A list to track all of the sockets
+var SOCKET_LIST = {};
 
-
+//Creating the io object (to help us talk with the sockets)
 var io = require('socket.io')(serv,{});
 
+//Setting up what to do when someone connects
 io.sockets.on('connection', function(socket){
-
+    
+    //Give it an ID and add it to the list
 	console.log('socket connection');
 	socket.id = Math.random();
 	SOCKET_LIST[socket.id] = socket;
-
-	socket.on('signIn', function(data){
-
-		Player.onConnect(socket, data.username);
-		socket.emit('signInResponse',{success:true});
-	});
+    
+    //Determine what to do when it disconnects
+    socket.on('disconnect', function(){
         
-    	socket.on('disconnect', function(){
-		Player.onDisconnect(socket);
 		delete SOCKET_LIST[socket.id];
 	});
 	
 });
 
 
-Board.update = function(){
-    var pack = [];
-}
 
 
+//Every so often, send data back to the players
 setInterval(function(){
     
-    pack = Board.update();
+    pack = "Test";
     
     for(var i in SOCKET_LIST){
 		
         
 				var socket = SOCKET_LIST[i];
 				socket.emit('update', pack);
-			}
-		}
 	}
 }, 1000/25);    
     
